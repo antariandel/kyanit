910984aa107d6d651f501299e9f9373ce1561f13
Zsolt Nagy <zsolt@kyanit.eu>
2020-09-22 21:28:41 +0200

    build: update manifest.py to be compatible with latest micropython

--- mpbuild/manifest.py
+++ mpbuild/manifest.py
@@ -1,20 +1,22 @@
-freeze("$(PORT_DIR)/boards/KYANIT/modules")  # noqa
-
-# drivers
-freeze("$(MPY_DIR)/drivers/display", "ssd1306.py")  # noqa
+# freeze("$(MPY_DIR)/tools", ("upip.py", "upip_utarfile.py"))
 freeze("$(MPY_DIR)/drivers/dht", "dht.py")  # noqa
 freeze("$(MPY_DIR)/drivers/onewire")  # noqa
-
-# file utilities
-freeze("$(MPY_LIB_DIR)/upysh", "upysh.py")  # noqa
 
 # uasyncio
 include("$(MPY_DIR)/extmod/uasyncio/manifest.py")  # noqa
 
-# requests
-freeze("$(MPY_LIB_DIR)/urequests", "urequests.py")  # noqa
-freeze("$(MPY_LIB_DIR)/urllib.urequest", "urllib/urequest.py")  # noqa
+# drivers
+freeze("$(MPY_DIR)/drivers/display", "ssd1306.py")  # noqa
 
-# umqtt
-freeze("$(MPY_LIB_DIR)/umqtt.simple", "umqtt/simple.py")  # noqa
-freeze("$(MPY_LIB_DIR)/umqtt.robust", "umqtt/robust.py")  # noqa
+# Libraries from micropython-lib, include only if the library directory exists
+if os.path.isdir(convert_path("$(MPY_LIB_DIR)")):  # noqa
+    # file utilities
+    freeze("$(MPY_LIB_DIR)/upysh", "upysh.py")  # noqa
+
+    # requests
+    freeze("$(MPY_LIB_DIR)/urequests", "urequests.py")  # noqa
+    freeze("$(MPY_LIB_DIR)/urllib.urequest", "urllib/urequest.py")  # noqa
+
+    # umqtt
+    freeze("$(MPY_LIB_DIR)/umqtt.simple", "umqtt/simple.py")  # noqa
+    freeze("$(MPY_LIB_DIR)/umqtt.robust", "umqtt/robust.py")  # noqa
